b37946053467beaab7ba7df2ed78a3a39882fd6b
Gwin27 <uxdan@mail.ru>
2021-11-04 16:58:12 +0300

    Create ColorConsole.cs

---
+++ ConsoleProg/ConsoleApp1/ColorConsole.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ColorCnsl1
+{
+    public class ColorCnsl
+    {
+        public static void Write(string msg)
+        {
+            string[] ss = msg.Split('{', '}');
+            ConsoleColor c;
+            foreach (var s in ss)
+                if (s.StartsWith("/"))
+                    Console.ResetColor();
+                else if (s.StartsWith("=") && Enum.TryParse(s.Substring(1), out c))
+                    Console.ForegroundColor = c;
+                else
+                    Console.Write(s);
+        }
+    }
+}
