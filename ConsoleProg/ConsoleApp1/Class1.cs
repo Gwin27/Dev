06fdd20bf6a1669fa6609bb4cdd0398425dce250
Gwin27 <uxdan@mail.ru>
2021-11-04 16:57:55 +0300

    Create Class1.cs

---
+++ ConsoleProg/ConsoleApp1/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using ColorCnsl1;
+
+namespace HomeWork1
+{
+    public class ReadData : IExecutionAction
+    {
+        string IExecutionAction.ExecutionAction()
+        {
+            ReadFile();
+            return "Press Enter";
+        }
+        private static void ReadFile()
+        {
+            ColorCnsl.Write("{=Yellow}Enter file path {/}\n");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    Console.WriteLine(sr.ReadToEnd());
+                }
+            }
+            catch
+            {
+                ColorCnsl.Write("{=Red}Error. Invalid file path{/}\n");
+            }
+        }
+    }
+}
