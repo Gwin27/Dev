2a8ce048464ea71ce8146c30752591f89e830083
Gwin27 <uxdan@mail.ru>
2021-11-04 16:58:15 +0300

    Create Interface.cs

---
+++ ConsoleProg/ConsoleApp1/Interface.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HomeWork1
+{
+    interface IActionUser
+    {
+        void GetActionUser(IExecutionAction action);
+
+    }
+    interface IExecutionAction
+    {
+        string ExecutionAction();
+    }
+    class ActionUser : IActionUser
+    {
+        public void GetActionUser(IExecutionAction action)
+        {
+            Console.WriteLine(action.ExecutionAction());
+        }
+    }
+}
